2e63fb10b46933d9a5eff6f3f56fada730e7bc7c
KarenRoth <karenrothc@gmail.com>
2020-12-02 16:23:40 -0300

    layout ok

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+const baseURL = process.env.NODE_ENV === 'development' ?
+  'http://localhost:3000/' :
+  '/'
+
+  
+const authService = axios.create({baseURL, withCredentials: true })
+export const signupFn = userInfo =>
+  authService.post('/signup', userInfo)
+export const loginFn = userInfo =>
+authService.post('/login', userInfo)
+export const currentUserFn = () =>
+  authService.get('/current-user')
+export const logoutFn = () =>
+  authService.get('/logout')
