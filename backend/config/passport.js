0e4fb3d8b9dbb2339601ca4db3a54ba36086db72
ak <konarayhan@gmail.com>
2020-12-02 12:28:39 -0500

    ahora si

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -3,6 +3,8 @@ const passport = require('passport');
 
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
+/////////
 passport.deserializeUser(User.deserializeUser());
+//////
 
 module.exports = passport;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+//Exporta todos los archivos en Pages, para minimizar el codigo de Router.js
+
+export {default as Home} from './Home'
+export {default as NotFound} from './NotFound'
+export {default as Signup} from './Signup'
+export {default as Login} from './Login'
