e79f4b23b4b707e048283857c19bf89ba08f2971
ak <konarayhan@gmail.com>
2020-12-09 09:40:50 -0500

    react app in server

---
+++ backend/public/build/precache-manifest.f317c9d319f00af8231c80da19ab56ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2849d286178951989044af9e28ea4b22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "836191ccaf06351f280f",
+    "url": "/static/css/2.9d6f32cf.chunk.css"
+  },
+  {
+    "revision": "b88b425bef1be058873d",
+    "url": "/static/css/main.daac1ec2.chunk.css"
+  },
+  {
+    "revision": "836191ccaf06351f280f",
+    "url": "/static/js/2.918c0db7.chunk.js"
+  },
+  {
+    "revision": "b88b425bef1be058873d",
+    "url": "/static/js/main.025f16aa.chunk.js"
+  },
+  {
+    "revision": "fb47f3c02db3ce888eb4",
+    "url": "/static/js/runtime-main.dc128e9d.js"
+  }
+]);
